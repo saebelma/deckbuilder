696ecd6065d7282f275975fd1197adc2ecc714e1
Markus Säbel <markus.saebel@gmail.com>
2022-12-21 19:11:22 +0100

    Manual commit

--- src/main/java/com/example/application/data/Deck.java
+++ src/main/java/com/example/application/data/Deck.java
@@ -2,21 +2,44 @@ package com.example.application.data;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import lombok.Data;
 
 @Data
 public class Deck {
 
-    private String name;
-    private List<Slot> decklist = new ArrayList<>();
+	private String name;
+	private List<Slot> mainboard = new ArrayList<>();
+	private List<Slot> sideboard = new ArrayList<>();
 
-    public void addCard(Card card) {
-        Slot newSlot = new Slot(card, 1);
-        if (!decklist.contains(newSlot)) decklist.add(newSlot);
-    }
+	public void addSlotToMainboard(Card card) {
+		Slot newSlot = new Slot(card, 1);
+		if (!mainboard.contains(newSlot))
+			mainboard.add(newSlot);
+	}
 
-    public void deleteSlot(Slot slot) {
-        decklist.remove(slot);
-    }
+	public void deleteSlotFromMainboard(Slot slot) {
+		mainboard.remove(slot);
+	}
+
+	public void addSlotToSideboard(Card card) {
+		Slot newSlot = new Slot(card, 1);
+		if (!sideboard.contains(newSlot))
+			sideboard.add(newSlot);
+	}
+
+	public void deleteSlotFromSideboard(Slot slot) {
+		sideboard.remove(slot);
+	}
+
+	public String getText() {
+		String text = "Mainboard\n";
+		text += getMainboard().stream().map(slot -> slot.getNumberOfCopies() + " " + slot.getCard().getCardName())
+				.collect(Collectors.joining("\n"));
+		text += "\n\nSideboard\n";
+		text += getSideboard().stream().map(slot -> slot.getNumberOfCopies() + " " + slot.getCard().getCardName())
+				.collect(Collectors.joining("\n"));
+		return text;
+	}
 }
--- src/main/java/com/example/application/views/helloworld/DeckBuilderView.java
+++ src/main/java/com/example/application/views/helloworld/DeckBuilderView.java
@@ -1,5 +1,19 @@
 package com.example.application.views.helloworld;
 
+import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.boot.json.JacksonJsonParser;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+import org.vaadin.olli.FileDownloadWrapper;
+
+import com.example.application.data.Card;
+import com.example.application.data.Deck;
+import com.example.application.data.Slot;
+import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Image;
@@ -11,137 +25,169 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.StreamResource;
 
-import java.io.ByteArrayInputStream;
-import java.util.*;
-import java.util.stream.Collectors;
-
-import org.springframework.boot.json.JacksonJsonParser;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
-import org.vaadin.olli.FileDownloadWrapper;
-
-import com.example.application.data.*;
-
 @PageTitle("Swifty Deckbuilder")
 @Route(value = "")
 public class DeckBuilderView extends VerticalLayout {
-    Grid<Card> searchResults = new Grid<>(Card.class);
-    Image cardImage = new Image();
-    Deck deck = new Deck();
-    Grid<Slot> deckView = new Grid<>(Slot.class, false);
+	Grid<Card> searchResults = new Grid<>(Card.class);
+	Image cardImage = new Image();
+	Deck deck = new Deck();
+	Grid<Slot> mainboardView = new Grid<>(Slot.class, false);
+	Grid<Slot> sideboardView = new Grid<>(Slot.class, false);
 
-    public DeckBuilderView() {
+	public DeckBuilderView() {
 
-        // Generate Scryfall search bar
-        TextField textField = new TextField("Scryfall Search");
-        Button searchButton = new Button("Go");
-        searchButton.addClickListener(
-                click -> searchScryfall(textField.getValue()));
-        HorizontalLayout searchBar = new HorizontalLayout(textField,
-                searchButton);
-        searchBar.setDefaultVerticalComponentAlignment(Alignment.BASELINE);
+		// Generate search bar and results view
+		addCardSearchView();
 
-        // Generate grid for displaying search results
-        searchResults.setColumns("cardName", "manaCost", "type");
-        searchResults.addColumn(new ComponentRenderer<>(
-                card -> new Button("Add to Deck", event -> {
-                    deck.addCard(card);
-                    updateDeckView();
-                })));
-        searchResults.asSingleSelect()
-                .addValueChangeListener(event -> showCard(event.getValue()));
-        searchResults.setHeight("400px");
+		// Generate grid for displaying and editing mainboard
+		addMainBoardView();
 
-        cardImage.setHeight("400px");
-        HorizontalLayout searchResultsView = new HorizontalLayout(searchResults,
-                cardImage);
-        searchResultsView
-                .setDefaultVerticalComponentAlignment(Alignment.BASELINE);
-        searchResultsView.setWidthFull();
+		// Generate grid for displaying and editing sideboard
+		addSideboardView();
 
-        // Assemble and add card search view
-        VerticalLayout cardSearchView = new VerticalLayout(searchBar,
-                searchResultsView);
-        add(cardSearchView);
+		// Add download button
+		addDownloadButton();
 
-        // Generate grid for displaying and editing decklist
-        deckView.addColumn(slot -> slot.getCard().getCardName())
-                .setHeader("Card Name");
-        deckView.addColumn(slot -> slot.getCard().getManaCost())
-                .setHeader("Mana Cost");
-        deckView.addColumn(slot -> slot.getCard().getType()).setHeader("Type");
-        deckView.addColumn("numberOfCopies");
-        deckView.addColumn(
-                new ComponentRenderer<>(slot -> new Button("+", event -> {
-                    slot.addCopy();
-                    updateDeckView();
-                })));
-        deckView.addColumn(
-                new ComponentRenderer<>(slot -> new Button("-", event -> {
-                    slot.subtractCopy();
-                    updateDeckView();
-                })));
-        deckView.asSingleSelect().addValueChangeListener(event -> {
-            if (event.getValue() != null) showCard(event.getValue().getCard());
-        });
-        updateDeckView();
+	}
 
-        add(deckView);
+	private void addCardSearchView() {
+		HorizontalLayout searchBar = getSearchBar();
+		HorizontalLayout searchResultsView = getSearchResultsView();
+		add(new VerticalLayout(searchBar, searchResultsView));
+	}
 
-        Button button = new Button("Download Decklist");
-        FileDownloadWrapper buttonWrapper = new FileDownloadWrapper(
-                new StreamResource("decklist.txt",
-                        () -> new ByteArrayInputStream(deck.getDecklist()
-                                .stream()
-                                .map(slot -> slot.getNumberOfCopies() + " "
-                                        + slot.getCard().getCardName())
-                                .collect(Collectors.joining("\n"))
-                                .getBytes())));
-        buttonWrapper.wrapComponent(button);
-        add(buttonWrapper);
+	private void addDownloadButton() {
+		Button button = new Button("Download Decklist");
+		FileDownloadWrapper buttonWrapper = new FileDownloadWrapper(
+				new StreamResource("decklist.txt", () -> new ByteArrayInputStream(deck.getText().getBytes())));
+		buttonWrapper.wrapComponent(button);
+		add(buttonWrapper);
+	}
 
-    }
+	private HorizontalLayout getSearchResultsView() {
+		searchResults.setColumns("cardName", "manaCost", "type");
+		searchResults.addColumn(new ComponentRenderer<>(card -> new Button("Add to Mainboard", event -> {
+			deck.addSlotToMainboard(card);
+			updateMainboardView();
+		})));
+		searchResults.addColumn(new ComponentRenderer<>(card -> new Button("Add to Sideboard", event -> {
+			deck.addSlotToSideboard(card);
+			updateSideboardView();
+		})));
+		searchResults.asSingleSelect().addValueChangeListener(event -> showCard(event.getValue()));
+		searchResults.setHeight("400px");
 
-    private void showCard(Card value) {
-        if (value != null) cardImage.setSrc(value.getImageURL());
-    }
+		cardImage.setHeight("400px");
+		if (cardImage.getSrc() == "")
+			cardImage.setSrc("images/Magic_card_back.webp");
+		HorizontalLayout searchResultsView = new HorizontalLayout(searchResults, cardImage);
+		searchResultsView.setDefaultVerticalComponentAlignment(Alignment.BASELINE);
+		searchResultsView.setWidthFull();
+		return searchResultsView;
+	}
 
-    private void updateDeckView() {
-        deckView.setItems(deck.getDecklist());
-    }
+	private HorizontalLayout getSearchBar() {
+		TextField textField = new TextField("Scryfall Search");
+		Button searchButton = new Button("Go");
+		searchButton.addClickListener(click -> searchScryfall(textField.getValue()));
+		searchButton.addClickShortcut(Key.ENTER);
+		HorizontalLayout searchBar = new HorizontalLayout(textField, searchButton);
+		searchBar.setDefaultVerticalComponentAlignment(Alignment.BASELINE);
+		return searchBar;
+	}
 
-    private void searchScryfall(String value) {
-        List<Card> cardList = new ArrayList<>();
+	private void addSideboardView() {
+		sideboardView.addColumn(slot -> slot.getCard().getCardName()).setHeader("Card Name");
+		sideboardView.addColumn(slot -> slot.getCard().getManaCost()).setHeader("Mana Cost");
+		sideboardView.addColumn(slot -> slot.getCard().getType()).setHeader("Type");
+		sideboardView.addColumn("numberOfCopies");
+		sideboardView.addColumn(new ComponentRenderer<>(slot -> new Button("+", event -> {
+			slot.addCopy();
+			updateSideboardView();
+		})));
+		sideboardView.addColumn(new ComponentRenderer<>(slot -> new Button("-", event -> {
+			slot.subtractCopy();
+			updateSideboardView();
+		})));
+		sideboardView.addColumn(new ComponentRenderer<>(slot -> new Button("Remove", event -> {
+			deck.deleteSlotFromSideboard(slot);
+			updateSideboardView();
+		})));
+		sideboardView.asSingleSelect().addValueChangeListener(event -> {
+			if (event.getValue() != null)
+				showCard(event.getValue().getCard());
+		});
+		updateSideboardView();
+		add(sideboardView);
+	}
 
-        try {
-            // Create rest template
-            RestTemplate rest = new RestTemplate();
+	private void addMainBoardView() {
+		mainboardView.addColumn(slot -> slot.getCard().getCardName()).setHeader("Card Name");
+		mainboardView.addColumn(slot -> slot.getCard().getManaCost()).setHeader("Mana Cost");
+		mainboardView.addColumn(slot -> slot.getCard().getType()).setHeader("Type");
+		mainboardView.addColumn("numberOfCopies");
+		mainboardView.addColumn(new ComponentRenderer<>(slot -> new Button("+", event -> {
+			slot.addCopy();
+			updateMainboardView();
+		})));
+		mainboardView.addColumn(new ComponentRenderer<>(slot -> new Button("-", event -> {
+			slot.subtractCopy();
+			updateMainboardView();
+		})));
+		mainboardView.addColumn(new ComponentRenderer<>(slot -> new Button("Remove", event -> {
+			deck.deleteSlotFromMainboard(slot);
+			updateMainboardView();
+		})));
+		mainboardView.asSingleSelect().addValueChangeListener(event -> {
+			if (event.getValue() != null)
+				showCard(event.getValue().getCard());
+		});
+		updateMainboardView();
+		add(mainboardView);
+	}
 
-            // Get response from scryfall
-            String jsonString = rest.getForObject(
-                    "https://api.scryfall.com/cards/search?q=" + value,
-                    String.class);
+	private void showCard(Card value) {
+		if (value != null)
+			cardImage.setSrc(value.getImageURL());
+	}
 
-            // Parse list of objects
-            JacksonJsonParser parser = new JacksonJsonParser();
-            Map<String, Object> map = parser.parseMap(jsonString);
-            List<Object> cards = (List<Object>) map.get("data");
+	private void updateMainboardView() {
+		mainboardView.setItems(deck.getMainboard());
+	}
 
-            // Parse card names
-            for (Object card : cards) {
-                Map<String, Object> cardProperties = (Map<String, Object>) card;
-                Card newCard = new Card();
-                newCard.setCardName((String) cardProperties.get("name"));
-                newCard.setImageURL((String) cardProperties.get("uri")
-                        + "?format=image&version=normal");
-                newCard.setManaCost((String) cardProperties.get("mana_cost"));
-                newCard.setType((String) cardProperties.get("type_line"));
-                cardList.add(newCard);
-            }
-        } catch (RestClientException e) {
-            e.printStackTrace();
-        }
+	private void updateSideboardView() {
+		sideboardView.setItems(deck.getSideboard());
+	}
 
-        searchResults.setItems(cardList);
-    }
+	private void searchScryfall(String value) {
+		List<Card> cardList = new ArrayList<>();
+
+		try {
+			// Create rest template
+			RestTemplate rest = new RestTemplate();
+
+			// Get response from scryfall
+			String jsonString = rest.getForObject("https://api.scryfall.com/cards/search?q=" + value, String.class);
+
+			// Parse list of objects
+			JacksonJsonParser parser = new JacksonJsonParser();
+			Map<String, Object> map = parser.parseMap(jsonString);
+			List<Object> cards = (List<Object>) map.get("data");
+
+			// Parse card names
+			for (Object card : cards) {
+				Map<String, Object> cardProperties = (Map<String, Object>) card;
+				Card newCard = new Card();
+				newCard.setCardName((String) cardProperties.get("name"));
+				newCard.setImageURL((String) cardProperties.get("uri") + "?format=image&version=normal");
+				newCard.setManaCost((String) cardProperties.get("mana_cost"));
+				newCard.setType((String) cardProperties.get("type_line"));
+				cardList.add(newCard);
+			}
+		} catch (RestClientException e) {
+			e.printStackTrace();
+		}
+
+		searchResults.setItems(cardList);
+	}
 }
